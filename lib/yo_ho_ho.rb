c91f2f7ddb0b50f364d3cb2bc26b9d581bbf9249
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2020-05-13 15:41:43 +1000

    add Rack response

--- lib/yo_ho_ho.rb
+++ lib/yo_ho_ho.rb
@@ -11,7 +11,12 @@ module YoHoHo
       klass, action = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(action)
-      [200, { 'Content-Type' => 'text/html' }, [text]]
+      response = controller.get_response
+      if response
+        [response.status, response.headers, [response.body].flatten]
+      else
+        [200, { 'Content-Type' => 'text/html' }, [text]]
+      end
     end
   end
 end
--- lib/yo_ho_ho/controller.rb
+++ lib/yo_ho_ho/controller.rb
@@ -22,6 +22,21 @@ module YoHoHo
       request.params
     end
 
+    def response(text, status = 200, headers = {})
+      raise 'Already responded!' if @response
+
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+
+    def get_response
+      @response
+    end
+
+    def render_response(*args)
+      response(render(*args))
+    end
+
     def render(view_name, locals = {})
       filename = File.join 'app', 'views', controller_name, "#{view_name}.html.erb"
       template = File.read filename
